5dc6803289581657ff5e9f38008c5a1227a186d3
Rafael <rafaelmagalhaes1@live.com>
2023-09-01 08:00:29 -0300

    refat: component Album

--- src/pages/Album.tsx
+++ src/pages/Album.tsx
@@ -1,10 +1,10 @@
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import getMusics from '../services/musicsAPI';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 import Loading from '../components/Loading';
 import MusicCard from '../components/MusicCard';
 import { AlbumType, SongType } from '../types';
-import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 export default function Album() {
   const { id } = useParams<{ id: string }>();
@@ -14,50 +14,52 @@ export default function Album() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchDataAPI = async () => {
       if (id) {
-        setLoading(true);
         try {
+          setLoading(true);
           const results = await getMusics(id);
+          const favoriteSongsData = await getFavoriteSongs();
           const [albumData, ...songsData] = results;
           setAlbum(albumData);
           setSongs(songsData);
-          const favoriteSongsData = await getFavoriteSongs();
           setFavoriteSongs(favoriteSongsData);
         } catch (error) {
           console.error('Error fetching data!', error);
         } finally {
           setLoading(false);
         }
-      } else console.error('ID inválido ou inexistente');
+      } else {
+        console.error('ID inválido ou inexistente');
+      }
     };
-    fetchData();
+    fetchDataAPI();
   }, [id]);
 
+  if (loading) return <Loading />;
+
   return (
-    <div>
-      {loading ? (
-        <Loading />
-      ) : (
-        <div className="album-details">
-          <img src={ album?.artworkUrl100 } alt="capa álbum" />
-          <h2 data-testid="artist-name">{album?.artistName}</h2>
-          <h1 data-testid="album-name">{album?.collectionName}</h1>
-          <div className="music-list">
-            {songs.map((song) => (
-              <div key={ song.trackId }>
-                <MusicCard
-                  trackId={ song.trackId }
-                  trackName={ song.trackName }
-                  previewUrl={ song.previewUrl }
-                  favoriteTrack={ favoriteSongs
-                    .some((favoriteSong) => favoriteSong.trackId === song.trackId) }
-                />
-              </div>
-            ))}
+    <div className="album-details">
+      <div className="album-cover">
+        <img src={ album?.artworkUrl100 } alt="capa álbum" />
+      </div>
+      <div className="album-title">
+        <h1 data-testid="album-name">{album?.collectionName}</h1>
+        <h2 data-testid="artist-name">{album?.artistName}</h2>
+      </div>
+      <div className="music-list">
+        {songs.map((song) => (
+          <div key={ song.trackId }>
+            <MusicCard
+              trackId={ song.trackId }
+              trackName={ song.trackName }
+              previewUrl={ song.previewUrl }
+              favoriteTrack={ favoriteSongs
+                .some((favoriteSong) => favoriteSong.trackId === song.trackId) }
+            />
           </div>
-        </div>
-      )}
+        ))}
+      </div>
     </div>
   );
 }
