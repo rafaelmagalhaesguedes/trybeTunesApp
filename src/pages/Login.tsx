646a8ab98a673e3b23849d47ea6dcd1bfe1ae2a1
Rafael <rafaelmagalhaes1@live.com>
2023-09-01 22:37:03 -0300

    style: component Login styled

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { createUser } from '../services/userAPI';
 import Loading from '../components/Loading';
+import '../css/login.css';
+import Logo from '../images/logo.png';
 
 export default function Login() {
   const navigate = useNavigate();
@@ -29,31 +31,33 @@ export default function Login() {
   if (loading) return <Loading />;
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form>
-        <div className="name-input">
-          <label htmlFor="name">Nome</label>
-          <input
-            data-testid="login-name-input"
-            id="name"
-            name="name"
-            value={ nameValue }
-            onChange={ handleChangeName }
-            required
-          />
+    <main className="container-login">
+      <section className="section-login">
+        <div className="login">
+          <img className="img-logo" src={ Logo } alt="Logo" />
+          <form className="form-login">
+            <input
+              className="input-login"
+              data-testid="login-name-input"
+              id="name"
+              name="name"
+              value={ nameValue }
+              onChange={ handleChangeName }
+              placeholder="qual é o seu nome?"
+              required
+            />
+            <button
+              className="button-login"
+              data-testid="login-submit-button"
+              type="submit"
+              disabled={ !validaInput }
+              onClick={ handleSubmit }
+            >
+              Entrar
+            </button>
+          </form>
         </div>
-        <div className="submit-button">
-          <button
-            data-testid="login-submit-button"
-            type="submit"
-            disabled={ !validaInput }
-            onClick={ handleSubmit }
-          >
-            Entrar
-          </button>
-        </div>
-      </form>
-    </div>
+      </section>
+    </main>
   );
 }
