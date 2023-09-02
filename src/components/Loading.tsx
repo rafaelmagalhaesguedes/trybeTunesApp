1c49164f968e6efed2cc9afec06c018886a575bf
Rafael <rafaelmagalhaes1@live.com>
2023-09-02 09:21:36 -0300

    style: add spinner component Loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,12 +1,13 @@
 /*
 *  Component Loading
 */
+import '../css/loading.css';
 
 export default function Loading() {
   return (
     <>
       <p className="spinner" />
-      <p>Carregando...</p>
+      <p className="text-spinner">Carregando...</p>
     </>
   );
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { createUser } from '../services/userAPI';
-import Loading from '../components/Loading';
 import '../css/login.css';
 import Logo from '../images/logo.png';
 
@@ -28,35 +27,42 @@ export default function Login() {
     }
   };
 
-  if (loading) return <Loading />;
-
   return (
     <main className="container-login">
       <section className="section-login">
-        <div className="login">
-          <img className="img-logo" src={ Logo } alt="Logo" />
-          <form className="form-login">
-            <input
-              className="input-login"
-              data-testid="login-name-input"
-              id="name"
-              name="name"
-              value={ nameValue }
-              onChange={ handleChangeName }
-              placeholder="qual é o seu nome?"
-              required
-            />
-            <button
-              className="button-login"
-              data-testid="login-submit-button"
-              type="submit"
-              disabled={ !validaInput }
-              onClick={ handleSubmit }
-            >
-              Entrar
-            </button>
-          </form>
-        </div>
+        {!loading ? (
+          <div className="login">
+            <img className="img-logo" src={ Logo } alt="Logo" />
+            <form className="form-login">
+              <input
+                className="input-login"
+                data-testid="login-name-input"
+                id="name"
+                name="name"
+                value={ nameValue }
+                onChange={ handleChangeName }
+                placeholder="qual é o seu nome?"
+                required
+              />
+              <button
+                className="button-login"
+                data-testid="login-submit-button"
+                type="submit"
+                disabled={ !validaInput }
+                onClick={ handleSubmit }
+              >
+                Entrar
+              </button>
+            </form>
+          </div>
+        ) : (
+          <div className="loading-login">
+            <div className="spinner-login" />
+            <div className="spinner-text">
+              <p>Carregando...</p>
+            </div>
+          </div>
+        )}
       </section>
     </main>
   );
