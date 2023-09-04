33e28b71a521dbf4c6a7006a2924286b5d044296
Rafael <rafaelmagalhaes1@live.com>
2023-09-04 10:57:27 -0300

    style: component ProfileEdit styled

--- src/pages/ProfileEdit.tsx
+++ src/pages/ProfileEdit.tsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleExclamation } from '@fortawesome/free-solid-svg-icons';
 import { UserType } from '../types';
 import { getUser, updateUser } from '../services/userAPI';
 import Loading from '../components/Loading';
+import '../css/profileEdit.css';
 
 function ProfileEdit() {
   const navigate = useNavigate();
@@ -19,13 +22,34 @@ function ProfileEdit() {
     setUser((prevData) => ({ ...prevData, [name]: value }));
   };
 
+  const handleChangeTextArea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setUser((prevData) => ({ ...prevData, [name]: value }));
+  };
+
   const validateInputs = () => {
     const userName = user.name.length > 0;
-    const userEmail = user.email.length > 0 && /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
+    const userEmail = user.email.length > 0 && validateEmail();
     const userDescription = user.description.length > 0;
     const userImage = user.image.length > 0;
     if (userName && userEmail && userDescription && userImage) {
       return true;
+    }
+  };
+
+  const validateEmail = () => {
+    const emails = user.email;
+    const emailRegex = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b/;
+    return emailRegex.test(emails);
+  };
+
+  const validateURL = () => {
+    const urls = user.image;
+    try {
+      const parsedURL = new URL(urls);
+      return parsedURL.protocol === 'https:';
+    } catch (error) {
+      return false;
     }
   };
 
@@ -56,70 +80,125 @@ function ProfileEdit() {
     fetchUserAPI();
   }, []);
 
-  if (loading) return <Loading />;
-
   return (
-    <div className="edit-profile-form">
-      <form className="form">
-        <div className="input-name">
-          <label htmlFor="name">Nome</label>
-          <input
-            data-testid="edit-input-name"
-            id="name"
-            name="name"
-            value={ user.name }
-            onChange={ handleChange }
-            required
-          />
-        </div>
+    <div className="container-edit-profile">
 
-        <div className="input-email">
-          <label htmlFor="email">Email</label>
-          <input
-            data-testid="edit-input-email"
-            id="email"
-            name="email"
-            value={ user.email }
-            onChange={ handleChange }
-            required
-          />
-        </div>
+      <header className="header-edit-profile" />
 
-        <div className="input-description">
-          <label htmlFor="description">Description</label>
-          <input
-            data-testid="edit-input-description"
-            id="description"
-            name="description"
-            value={ user.description }
-            onChange={ handleChange }
-            required
-          />
-        </div>
+      <main className="main-edit-profile">
 
-        <div className="input-image">
-          <label htmlFor="image">Image</label>
-          <input
-            data-testid="edit-input-image"
-            id="image"
-            name="image"
-            value={ user.image }
-            onChange={ handleChange }
-            required
-          />
-        </div>
+        {!loading ? (
+          <>
+            <div className="image-user-profile">
+              <img
+                className="image-user-edit-profile"
+                src={ user.image }
+                alt="ImageUserProfile"
+              />
+            </div>
 
-        <div className="button-save-form">
-          <button
-            data-testid="edit-button-save"
-            type="submit"
-            onClick={ handleSubmit }
-            disabled={ !validateInputs() }
-          >
-            Editar
-          </button>
-        </div>
-      </form>
+            <form className="form-edit-profile">
+
+              <div className="input-edit">
+                <label htmlFor="name">Nome</label>
+                <p>Fique à vontade para usar seu nome social</p>
+                <input
+                  data-testid="edit-input-name"
+                  id="name"
+                  name="name"
+                  value={ user.name }
+                  onChange={ handleChange }
+                  placeholder="Nome"
+                  required
+                />
+              </div>
+
+              <div className="input-edit-email">
+                <label htmlFor="email">E-mail</label>
+                <p>Escolha um e-mail que consulte diariamente</p>
+              </div>
+              <div className="input-icon-edit">
+                <input
+                  data-testid="edit-input-email"
+                  id="email"
+                  name="email"
+                  value={ user.email }
+                  onChange={ handleChange }
+                  placeholder="email@email.com"
+                  required
+                />
+                {validateEmail() ? (
+                  <FontAwesomeIcon
+                    className="icon-valid"
+                    icon={ faCircleExclamation }
+                  />
+                ) : (
+                  <FontAwesomeIcon
+                    className="icon-invalid"
+                    icon={ faCircleExclamation }
+                  />
+                )}
+              </div>
+
+              <div className="input-edit-description">
+                <label htmlFor="description">Description</label>
+                <textarea
+                  className="edit-input-description"
+                  data-testid="edit-input-description"
+                  id="description"
+                  name="description"
+                  value={ user.description }
+                  onChange={ handleChangeTextArea }
+                  placeholder="Sobre mim"
+                  required
+                />
+              </div>
+
+              <div className="input-edit-image-url">
+                <label htmlFor="image">Image url</label>
+              </div>
+              <div className="input-icon-edit-url">
+                <input
+                  data-testid="edit-input-image"
+                  id="image"
+                  name="image"
+                  value={ user.image }
+                  onChange={ handleChange }
+                  placeholder="Insira um link"
+                  required
+                />
+                {validateURL() ? (
+                  <FontAwesomeIcon
+                    className="icon-valid"
+                    icon={ faCircleExclamation }
+                  />
+                ) : (
+                  <FontAwesomeIcon
+                    className="icon-invalid"
+                    icon={ faCircleExclamation }
+                  />
+                )}
+              </div>
+
+              <div className="button-save-form">
+                <button
+                  data-testid="edit-button-save"
+                  type="submit"
+                  onClick={ handleSubmit }
+                  disabled={ !validateInputs() }
+                >
+                  Salvar
+                </button>
+              </div>
+
+            </form>
+          </>
+        ) : (
+          <div className="loading-profile-edit">
+            <Loading />
+          </div>
+        )}
+      </main>
     </div>
   );
 }
