8b7504d416f4670e6c349af017962a5741792551
Rafael <rafaelmagalhaes1@live.com>
2023-09-24 21:47:18 -0300

    fix: indentation globalStyles

--- src/globalStyles.tsx
+++ src/globalStyles.tsx
@@ -2,7 +2,8 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
   
-@import url('https://fonts.googleapis.com/css2?family=Epilogue:wght@400;500;700;900&family=Roboto:wght@400;500;900&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Epilogue:wght@400;500;700;900&family=Roboto:wght@400;500;900&display=swap');
+  
   * {
     margin: 0;
     padding: 0;
