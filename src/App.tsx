16a342fcf21f6ce143ab7e251f64528bf51cd3b8
Rafael <rafaelmagalhaes1@live.com>
2023-09-26 07:40:00 -0300

    fix: move globalStyles App to Main

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from 'react-router-dom';
-import { GlobalStyle } from './globalStyles';
 import {
   Login,
   Layout,
@@ -13,20 +12,17 @@ import {
 
 function App() {
   return (
-    <div className="app">
-      <GlobalStyle />
-      <Routes>
-        <Route path="/" element={ <Login /> } />
-        <Route path="/" element={ <Layout /> }>
-          <Route path="/search" element={ <Search /> } />
-          <Route path="/album/:id" element={ <Album /> } />
-          <Route path="/favorites" element={ <Favorites /> } />
-          <Route path="/profile" element={ <Profile /> } />
-          <Route path="/profileedit" element={ <ProfileEdit /> } />
-        </Route>
-        <Route path="*" element={ <PageNotFound /> } />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={ <Login /> } />
+      <Route path="/" element={ <Layout /> }>
+        <Route path="/search" element={ <Search /> } />
+        <Route path="/album/:id" element={ <Album /> } />
+        <Route path="/favorites" element={ <Favorites /> } />
+        <Route path="/profile" element={ <Profile /> } />
+        <Route path="/profileedit" element={ <ProfileEdit /> } />
+      </Route>
+      <Route path="*" element={ <PageNotFound /> } />
+    </Routes>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { GlobalStyle } from './globalStyles';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as Element);
 root.render(
   <BrowserRouter>
+    <GlobalStyle />
     <App />
   </BrowserRouter>,
 );
